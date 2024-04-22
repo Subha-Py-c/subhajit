137a5be4b554540a63d4f3b3985c54a05d28176c
Dreamy Developer <dreamy.star.368@gmail.com>
2024-04-22 15:20:55 +0530

    nuos

--- script.js
+++ script.js
@@ -236,3 +236,103 @@ function easeInOutCubic(t) {
   });
  });
  
+
+
+//  they will be displayed
+ const Default = document.querySelector('.default');
+ const python = document.querySelector('.python');
+ const ai_tools = document.querySelector('.ai-tools');
+ const html_css = document.querySelector('.html-css');
+ const ui_ux = document.querySelector('.ui-ux');
+ const js = document.querySelector('.js');
+//  const react = document.querySelector('.react');
+ const c_cpp = document.querySelector('.c-cpp');
+ const git = document.querySelector('.git');
+
+//  const card = document.querySelector('.card');
+
+// when these are hovered
+ const p = document.getElementById('python');
+ const a = document.getElementById('ai');
+ const htm = document.getElementById('html-css');
+ const u = document.getElementById('ui-ux');
+ const j = document.getElementById('js');
+//  const r = document.getElementById('react');
+ const c = document.getElementById('c');
+ const cpp = document.getElementById('cpp');
+ const g = document.getElementById('git');
+
+
+ p.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   python.style.display = 'block';
+ });
+
+ a.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   ai_tools.style.display = 'block';
+ });
+
+ htm.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   html_css.style.display = 'block';
+ });
+
+ u.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   ui_ux.style.display = 'block';
+ });
+
+ j.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   js.style.display = 'block';
+ });
+
+ c.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   c_cpp.style.display = 'block';
+ });
+
+ g.addEventListener('mouseover', function(){
+   Default.style.display = 'none';
+   git.style.display = 'block';
+ });
+
+
+
+
+ p.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   python.style.display = 'none';
+  //  python.classList.add('invisible');
+ });
+
+ a.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   ai_tools.style.display = 'none';
+ });
+
+ htm.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   html_css.style.display = 'none';
+ });
+
+ u.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   ui_ux.style.display = 'none';
+ });
+
+ j.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   js.style.display = 'none';
+ });
+
+ c.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   c_cpp.style.display = 'none';
+ });
+
+ g.addEventListener('mouseout', function(){
+   Default.style.display = 'block';
+   git.style.display = 'none';
+ });
