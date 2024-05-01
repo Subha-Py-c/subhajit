0fb07a6d0575614da8b1b9835fb638024f050662
Dreamy Developer <dreamy.star.368@gmail.com>
2024-05-01 21:31:39 +0530

    message

--- js/main.js
+++ js/main.js
@@ -241,26 +241,54 @@ const sp1 = document.getElementById('sp1');
 const sp2 = document.getElementById('sp2');
 const sp3 = document.getElementById('sp3');
 const sp4 = document.getElementById('sp4');
+
+const closeIcon = document.querySelector('.close-icon');
+const navMenu = document.querySelector('.nav-menu');
+
 svg.addEventListener('click', function() {
   nav.classList.toggle('visible');
   ul.classList.toggle('visible');
   l1.classList.toggle('animate-li');
   l1.style.animationDelay = '0ms';
   l2.classList.toggle('animate-li');
-  l2.style.animationDelay = '200ms';
+  l2.style.animationDelay = '100ms';
   l3.classList.toggle('animate-li');
-  l3.style.animationDelay = '400ms';
+  l3.style.animationDelay = '200ms';
   l4.classList.toggle('animate-li');
-  l4.style.animationDelay = '600ms';
+  l4.style.animationDelay = '300ms';
   if (nav.classList.contains('visible')) {
-      sp1.classList.add('appear');
-      sp2.classList.add('appear');
-      sp3.classList.add('appear');
-      sp4.classList.add('appear')
+    sp1.classList.add('appear');
+    sp2.classList.add('appear');
+    sp3.classList.add('appear');
+    sp4.classList.add('appear');
+    navMenu.style.display = 'none';
+    closeIcon.style.display = 'flex';
   } else {
-      sp1.classList.remove('appear');
-      sp2.classList.remove('appear');
-      sp3.classList.remove('appear');
-      sp4.classList.remove('appear')
+    sp1.classList.remove('appear');
+    sp2.classList.remove('appear');
+    sp3.classList.remove('appear');
+    sp4.classList.remove('appear');
+    closeIcon.style.display = 'none';
+    navMenu.style.display = 'flex';
   }
-})
+});
+
+closeIcon.addEventListener('click', function() {
+  nav.classList.remove('visible');
+  ul.classList.remove('visible');
+  sp1.classList.remove('appear');
+  sp2.classList.remove('appear');
+  sp3.classList.remove('appear');
+  sp4.classList.remove('appear');
+  closeIcon.style.display = 'none';
+  navMenu.style.display = 'flex';
+});
+
+const eBox = document.getElementById('email-box');
+
+eBox.addEventListener('mouseover', function() {
+  cursor.style.display = 'none';
+});
+eBox.addEventListener('mouseout', function() {
+  cursor.style.display = 'block';
+});
