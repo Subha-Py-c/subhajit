4c565394fd8101aaae71856adf708f6e7761e128
Subhajit Gorai <goraisubhajit@345.com>
2024-08-10 22:03:07 +0530

    hello font in mobile

--- src/js/functions/hello.js
+++ src/js/functions/hello.js
@@ -5,12 +5,6 @@ const hello = document.getElementById('hello');
 document.addEventListener('scroll', function(e){
   const target = d3;
   var scrolled = window.scrollY;
-  var rate = scrolled * (-0.22);
-  target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
-
-  // var rate2 = scrolled * (-0.22);
-  // const targets = document.querySelectorAll('.scroll');
-  // targets.forEach((el) => {
-  //   el.style.transform = 'translate3d(0px, ' + rate2 + 'px, 0px)';
-  // })
+  var rate = scrolled * (0.22);
+  target.style.transform = 'translate3d(' + rate + 'px, 0px, 0px)';
 });
