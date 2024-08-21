0a85fd05581e2952a75c2fd1cace687846ba3e5d
Subhajit Gorai <goraisubhajit@345.com>
2024-08-21 20:52:45 +0530

    nav fix

--- src/js/functions/nav-js/nav-appear-and-toggle-svg.js
+++ src/js/functions/nav-js/nav-appear-and-toggle-svg.js
@@ -1,53 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.getElementById("nav");
-  const ul = document.getElementById("ul");
   const navMenu = document.querySelector(".nav-menu");
   const lis = document.querySelectorAll(".li");
-  const sps = document.querySelectorAll(".sp");
   const closeIcon = document.querySelector(".close-icon");
 
 
-
-
-
-
-
   navMenu.addEventListener("click", function () {
-    nav.classList.toggle("visible");
-    ul.classList.toggle("visible");
     lis.forEach((li, index) => {
-      li.classList.toggle("animate-li");
-      li.style.animationDelay = `${index * 40}ms`;
+      li.classList.add("active");
     });
-    sps.forEach((sp) => sp.classList.toggle("appear"));
-    if (nav.classList.contains("visible")) {
-      navMenu.style.display = "none";
-      closeIcon.style.display = "flex";
-    } else {
-      closeIcon.style.display = "none";
-      navMenu.style.display = "flex";
-    }
-    nav.style.height = "50vh";
+
+    navMenu.style.display = "none";
+    closeIcon.style.display = "flex";
   });
-
-
-
-
-
-
 
 
   closeIcon.addEventListener("click", function () {
-    nav.classList.remove("visible");
-    ul.classList.remove("visible");
-    sps.forEach((sp) => sp.classList.remove("appear"));
-    closeIcon.style.display = "none";
+    lis.forEach((li, index) => {
+      li.classList.remove("active");
+    });
+    
     navMenu.style.display = "flex";
-    nav.style.height = "10vh";
+    closeIcon.style.display = "none";
   });
-
-
-
 
 
   var w = 5000;
