54136f5471bc47e209287404098c94740e76bc47
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-07-07 21:23:14 +0530

    refactor: updated projects section

--- src/components/Main/ProjectsSection/ProjectsSection.jsx
+++ src/components/Main/ProjectsSection/ProjectsSection.jsx
@@ -1,163 +1,92 @@
-import { useEffect, useRef, useState } from "react";
+import { ChevronRight } from "lucide-react";
 import "./newP.css";
-import projects from "./Projects";
-import { ExternalLink } from "lucide-react";
+import projects from "./projectsData";
 
 const ProjectsSection = () => {
-    const projectWrapperRef = useRef(null);
-    const [activeIndex, setActiveIndex] = useState(null);
+  return (
+    <section
+      id="s3-projects"
+      className="sec3 pb-[25vh] md:pb-[32vh] lg:pb-[40vh]"
+    >
+      {/* anchor div */}
+      <div id="div3" style={{ width: 0, height: 0, opacity: 0 }}></div>
 
-    // useEffect(() => {
-    //     const projectWrapper = projectWrapperRef.current;
-    //     const projectImgs = projectWrapper.querySelectorAll(".parallaxImgs");
-    //     const projectChildren = [...projectImgs];
-    //     let initialPositions = new Map();
-    //     const coeff2 = 0.05;
-
-    //     // Parallax scroll effect
-    //     const parallaxScrollHandler = () => {
-    //         projectChildren.forEach((child) => {
-    //             const initialTop = initialPositions.get(child);
-    //             const offsetY = (window.scrollY - initialTop) * coeff2;
-    //             child.style.transform = `translate3d(0, ${offsetY * 1.618}px, 0)`;
-    //         });
-    //     };
-
-    //     const startParallax = () => {
-    //         projectChildren.forEach((child) => {
-    //             initialPositions.set(
-    //                 child,
-    //                 child.getBoundingClientRect().top + window.scrollY,
-    //             );
-    //         });
-    //         window.addEventListener("scroll", parallaxScrollHandler);
-    //     };
-
-    //     const stopParallax = () => {
-    //         window.removeEventListener("scroll", parallaxScrollHandler);
-    //     };
-
-    //     // Intersection Observer
-    //     const obsv = new IntersectionObserver(
-    //         (entries) => {
-    //             entries.forEach((entry) => {
-    //                 if (entry.isIntersecting) {
-    //                     startParallax();
-    //                 } else {
-    //                     stopParallax();
-    //                 }
-    //             });
-    //         },
-    //         { threshold: 0.1 },
-    //     );
-
-    //     obsv.observe(projectWrapper);
-    //     startParallax();
-
-    //     return () => {
-    //         stopParallax();
-    //         obsv.disconnect();
-    //     };
-    // }, []);
-
-    return (
-        <section
-            id="s3-projects"
-            className="sec3 pb-[25vh] md:pb-[32vh] lg:pb-[40vh]"
+      <h4 id="EN" className="project-heading-container">
+        <span
+          id="project-heading"
+          className="exp underline-sp headerSpan header__texts"
+          style={{ display: "inline" }}
         >
-            {/* anchor div */}
-            <div id="div3" style={{ width: 0, height: 0, opacity: 0 }}></div>
+          Selected Projects
+        </span>
+      </h4>
 
-            <h4 id="EN" className="project-heading-container">
-                <span
-                    id="project-heading"
-                    className="exp underline-sp headerSpan header__texts"
-                    style={{ display: "inline" }}
-                >
-                    Selected Projects
-                </span>
-            </h4>
+      <div className="projects-wrapper full">
+        {projects.map((project) => (
+          <div
+            key={project.title}
+            className="group relative border-dashed border-b border-slate-700/30 pt-8 pb-16 px-4 md:px-8 cursor-default overflow-hidden hover:bg-[#F2E5BF]/20"
+          >
+            <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-8">
+              {/* Left : content */}
+              <div className="space-y-6">
+                <div className="flex items-center justify-between md:justify-start gap-0 md:gap-16">
+                  <h2 className="text-4xl md:text-5xl font-[400] font-[title] group-hover:bg-clip-text transition-all duration-500">
+                    {project.title}
+                  </h2>
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block opacity-100 md:opacity-0 group-hover:opacity-100
+                                transition-all duration-500 transform
+                                flexed f-gap-1 rounded-full
+                                box-content px-3 py-1
+                                border border-black/80 bg-black/80 text-sky-100
+                                hover:bg-transparent hover:text-black"
+                  >
+                    visit <ChevronRight className="size-5 text-current" />
+                  </a>
+                </div>
 
-            <div className="projects-wrapper full" ref={projectWrapperRef}>
-                {projects.map((project, idx) => (
-                    <div
-                        key={project.title}
-                        className="group relative border-b border-slate-700/30 pt-8 pb-16 px-4 md:px-8 cursor-default overflow-hidden hover:bg-[#F2E5BF]/20"
-                        onMouseEnter={() => setActiveIndex(idx)}
-                        onMouseLeave={() => setActiveIndex(null)}
-                    >
-                        <div className="grid md:grid-cols-2 gap-8">
-                            {/* Left Content */}
-                            <div className="space-y-6">
-                                <div className="flex items-center justify-between md:justify-start gap-0 md:gap-16">
-                                    <h2 className="text-3xl md:text-4xl font-[400] font-serif group-hover:bg-clip-text transition-all duration-500">
-                                        {project.title}
-                                    </h2>
-                                    <a
-                                        href={project.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="block opacity-100 md:opacity-0 group-hover:opacity-100
-                                        transition-all duration-500 transform
-                                        flexed f-gap-1 rounded-full
-                                        box-content px-3 py-1
-                                        border border-black/80 bg-black/80 text-sky-100
-                                        hover:bg-transparent hover:text-black"
-                                    >
-                                        visit{" "}
-                                        <ExternalLink className="size-5 text-current" />
-                                    </a>
-                                </div>
+                <p className="text-xl text-black/60 font-medium max-w-md">
+                  {project.summary}
+                </p>
 
-                                <p className="text-xl text-black/60 font-medium max-w-md">
-                                    {project.summary}
-                                </p>
+                <div className="space-y-6 opacity-100 md:opacity-0 group-hover:opacity-100 transition-all duration-500 delay-100">
+                  <p className="text-md lg:text-sm text-black/80 max-w-[65ch]">
+                    {project.description}
+                  </p>
 
-                                <div className="space-y-6 opacity-100 md:opacity-0 group-hover:opacity-100 transition-all duration-500 delay-100">
-                                    <p className="text-md lg:text-sm text-black/80">
-                                        {project.description}
-                                    </p>
+                  <div className="flex flex-wrap gap-1 md:gap-3">
+                    {project.technologies.map((tech, i) => (
+                      <span
+                        key={i}
+                        className="ta-ta px-4 py-2 text-sm border border-[#B2CD9C] bg-[#B2CD9C] rounded-full hover:bg-[#00ac94]/100 transition-colors"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
 
-                                    <div className="flex flex-wrap gap-1 md:gap-3">
-                                        {project.technologies.map((tech, i) => (
-                                            <span
-                                                key={i}
-                                                className="px-4 py-2 text-sm border border-[#00ac94] bg-[#00ac94]/60 rounded-full hover:bg-[#00ac94]/100 transition-colors"
-                                            >
-                                                {tech}
-                                            </span>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* Right Content - Image */}
-                            <div className="relative mt-6 h-64 md:h-96 overflow-hidden rounded-lg ta-ta">
-                                <div className="absolute flex items-center justify-center inset-0 transform transition-all duration-700 overflow-hidden">
-                                    <img
-                                        src={project.image}
-                                        className="parallaxImgs object-cover size-full transform transition-transform duration-700 group-hover:scale-110"
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-500" />
-                            </div>
-                        </div>
-
-                        {/* Project Type Badge */}
-                        {/* <div
-                            className="absolute top-8 right-8 px-4 py-2
-                            border border-white/20 rounded-full text-sm
-                            opacity-0 group-hover:opacity-100 transition-all duration-500 delay-200
-                            bg-[#257180] text-white"
-                        >
-                            {project.type}
-                        </div> */}
-                    </div>
-                ))}
+              {/* Right : image */}
+              <div className="relative mt-6 h-64 aspect-video overflow-hidden ta-ta bg-[#dcc2bb] rounded-lg opacity-0 transform transition-all duration-700 group-hover:opacity-100">
+                <div className="absolute flex items-center justify-center inset-0 transform transition-all duration-700 overflow-hidden">
+                  <img
+                    src={project.image}
+                    className="parallaxImgs rounded-lg object-contain size-full transform transition-transform duration-700 scale-105"
+                    alt={project.title + " project image"}
+                  />
+                </div>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsSection;
--- src/components/Main/ProjectsSection/projectsData.js
+++ src/components/Main/ProjectsSection/projectsData.js
@@ -3,19 +3,26 @@ const projects = [
     title: "OpenCanvas",
     summary: "A site for art-lovers to browse and share all types of arts.",
     description: "Currently IN MAKING.",
-    type: "Personal project",
-    technologies: ["Next.js"],
+    technologies: ["Next.js", "React", "TailwindCSS"],
     link: "https://www.opencanvas.blog",
     image: "/images/projects-imgs/opencanvas.jpeg",
   },
 
-  // dsa-visualiser
+  {
+    title: "DSA Animations",
+    summary: "Data Structures and Algorithms visualiser",
+    description:
+      "An interactive web platform for visualizing Data Structures and Algorithms through animations. Built to make learning DSA concepts intuitive and engaging.",
+    technologies: ["React", "TailwindCSS"],
+    link: "https://dsa-experiments.vercel.app",
+    image: "/images/projects-imgs/dsa.png",
+  },
+
   {
     title: "myClassroom",
     summary: "Distraction-free platform for focused selfstudy",
     description:
       "webapp to create courses from youtube playlists. Design own courses, study without any ads or distractions. Track course progress, to-do, goals, study hours.",
-    type: "Personal project",
     technologies: [
       "flask",
       "yt_dlp",
@@ -31,13 +38,11 @@ const projects = [
 
   {
     title: "Zentunes",
-    summary: "Beautiful collection of some special genres of Music.",
-    description:
-      "A music streaming platform for retro lovers. Some development is yet to do, sorry!!",
-    type: "Personal Project",
-    technologies: ["HTML", "CSS", "JavaScript", "SCSS"],
-    link: "https://zentunes.netlify.app",
-    image: "/images/projects-imgs/krishisearch.jpeg",
+    summary: "Android Audio Player Application",
+    description: "A music streaming platform for retro lovers.",
+    technologies: ["Capacitor", "React", "SCSS"],
+    link: "https://github.com/Dream-World-Coder/zentunes/tree/android-app",
+    image: "/images/projects-imgs/zentunes.png",
   },
 
   {
@@ -45,11 +50,15 @@ const projects = [
     summary: "A neat platform for reading news.",
     description:
       "A rss reader with clutterless UI to read news without advertisements, along with A.I. Summary of each news available. Many other mindblowing features available.",
-    type: "Personal Project",
     technologies: [
-      "Backend: Flask, SQL(postgresql, sqlite)",
-      "Frontend: React, TailwindCSS",
-      "Web Scraping [python], A.I. Summary(API)",
+      "Flask",
+      "postgresql",
+      "sqlite",
+      "React",
+      "TailwindCSS",
+      "Dexie.js",
+      "Web Scraping",
+      "AI Summary(API)",
     ],
     link: "https://lumifeed.netlify.app",
     image: "/images/projects-imgs/lumifeed.jpeg",
@@ -60,7 +69,6 @@ const projects = [
     summary: "Highquality markdown, latex editor and pdf converter for free",
     description:
       "webapp to edit markdown documents and latex, insert images, links. Easily export to PDF/txt/md. completely free.",
-    type: "Personal project",
     technologies: ["React", "TailwindCSS", "jsPDF"],
     link: "https://www.opencanvas.blog/markdown2pdf",
     image: "/images/projects-imgs/md2pdf.jpeg",
@@ -71,8 +79,7 @@ const projects = [
     summary: "Encrypt messages or files easily.",
     description:
       "A powerful webapp to encrypt messages and files easily that are highly secure and cannot be decrypted without the encryption KEY.",
-    type: "Personal Project",
-    technologies: ["Flask", "Cryptography Module (python)", "JavaScript AJAX"],
+    technologies: ["Flask", "Cryptography", "AJAX"],
     link: "https://encryptease.pythonanywhere.com",
     image: "/images/projects-imgs/encryptease.jpeg",
   },
@@ -81,7 +88,6 @@ const projects = [
     title: "Portfolio",
     summary: "This portfolio website you are visiting right now.",
     description: "A portfolio Website to easily introduce myself to everyone.",
-    type: "Personal Project",
     technologies: ["React", "TailwindCSS", "GSAP", "figma"],
     link: "https://github.com/Subha-Py-c/subhajit",
     image: "/images/projects-imgs/portfolio.jpeg",
@@ -91,7 +97,6 @@ const projects = [
     title: "TithiRekha[তিথিরেখা]",
     summary: "Modern themed Bangla Calendar.",
     description: "A webapp to easily check bengali date and occations.",
-    type: "Personal Project",
     technologies: ["React", "TailwindCSS", "Web Scraping"],
     link: "https://bangla-calendar.vercel.app",
     image: "/images/projects-imgs/tithirekha.jpeg",
@@ -101,10 +106,9 @@ const projects = [
     title: "Maze Battle Game",
     summary: "Player vs CPU maze running game",
     description: "",
-    type: "Personal Project",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://mazebattle.netlify.app",
-    image: "/images/projects-imgs/relaxingmelody.jpeg",
+    image: "/images/projects-imgs/maze-battle.jpeg",
   },
 ];
 
